f7118b359d965429f8a123b5917057c16dc35e04
Kirill chEbba Chebunin <iam@chebba.org>
2011-11-03 00:14:22 +0400

    Add monolog adapter

---
+++ src/Che/LogStock/Adapter/MonologAdapter.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * Copyright (c) 2011 
+ * Kirill chEbba Cheunin <iam@chebba.org>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ */
+
+namespace Che\LogStock\Adapter;
+
+use Che\LogStock\Logger;
+
+/**
+ * Logger adapter for Monolog
+ * 
+ * @author Kirill chEbba Chebunin <iam@chebba.org>
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ */ 
+class MonologAdapter implements LoggerAdapter
+{
+    /**
+     * @var \Monolog\Logger
+     */
+    private $logger;
+
+    /**
+     * Constructor
+     *
+     * @param \Monolog\Logger $logger Monolog logger instance
+     */
+    public function __construct(\Monolog\Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function log($level, $message, array $context = array())
+    {
+        // Use ZF compatible methods to rely on Monolog internal level mapping
+        $method = self::getLevelMethod($level);
+        if (!$method) {
+            throw new \InvalidArgumentException("Unknown level '$level'");
+        }
+        call_user_func(array($this->logger, $method). $message, $context);
+    }
+
+    private static function getLevelMethod($level)
+    {
+        $levels = Logger::getLevels();
+        return isset($levels[$level]) ? strtolower($levels[$level]) : $levels[$level];
+    }
+}
